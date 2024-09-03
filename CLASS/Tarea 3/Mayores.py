899ead00a71a25962c330546c28f7e02d8c953d0
Diego Prado Pardo <dpradopardo@gmal.com>
2024-09-03 17:14:20 -0500

    Changes to be committed: new file:   CLASS/Tarea 3/Mayores.py Creacion de archivo

---
+++ CLASS/Tarea 3/Mayores.py
@@ -0,0 +1,20 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Programa: Números mayores a 100
+=========================================
+Fecha: 3 de Septiembre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
+def mayores_100(n1, n2, n3):
+
+    return function
+
+def main():
+    n1 = input('Ingrese un primer número diferente de cero: ')
+    n2 = input('Ingrese un segundo número diferente de cero: ')
+    n3 = input('Ingrese un tercer número diferente de cero: ')
+    return function
+main()
