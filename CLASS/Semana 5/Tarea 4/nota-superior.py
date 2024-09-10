2ff506cf6ef050b16b8127fd99c3deebc58b55a3
Diego Prado Pardo <dpradopardo@gmal.com>
2024-09-10 12:06:17 -0500

    modified

---
+++ CLASS/Semana 5/Tarea 4/nota-superior.py
@@ -0,0 +1,17 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Programa: Nota Superior
+=========================================
+Fecha: 10 de Septiembre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
+def promedio():
+    """Dados tres valores punto flotante, se calcula valor promedio,
+    (float, float, float) -> float"""  
+
+def main():
+    a=a
+main()
