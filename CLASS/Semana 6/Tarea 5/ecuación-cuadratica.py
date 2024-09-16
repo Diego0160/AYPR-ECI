ebe8e9b170bfc9a935c0ec196d8c765f67dd8d70
Diego Prado Pardo <dpradopardo@gmail.com>
2024-09-15 23:38:08 -0500

    modified files

---
+++ CLASS/Semana 6/Tarea 5/ecuación-cuadratica.py
@@ -0,0 +1,10 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Programa: 
+=========================================
+Fecha: 16 de Septiembre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
