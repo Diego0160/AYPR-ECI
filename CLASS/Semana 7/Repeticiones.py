5c98e5a7145f4cadb47cc791912374fbdbd42335
Diego Prado Pardo <dpradopardo@gmail.com>
2024-09-23 12:55:47 -0500

    some new fles

---
+++ CLASS/Semana 7/Repeticiones.py
@@ -0,0 +1,30 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Tema: Repeticiones
+=========================================
+Fecha: 23 de Septiembre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
+def saludo_recurrente(cont):
+    """ Imprime un saludo varias veces usando recurrencia
+    (int) -> None
+    """
+    print("Hola", cont)
+    if cont == 5:
+        print("Fin")
+    else:
+        cont += 1
+        saludo_recurrente(cont)
+
+def saludo_repetitivo(cont = 1):
+    """ Imprime un saludo de manera repetida usando iteracion
+    (int) -> None
+    """
+    while cont <= 5:
+        print("Hola", cont)
+        cont += 1
+    #end_while
+    print("Fin")
