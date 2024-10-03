d04241f08e8017b55fef4dc73107e2858ce28ad0
Diego Prado Pardo <dpradopardo@gmail.com>
2024-10-03 11:31:26 -0500

    some new file

---
+++ CLASS/Extra/diego.prado.py
@@ -0,0 +1,37 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Previa: Producto
+Tematica: Algorítmos con condicionales 
+          repetitivos
+=========================================
+Fecha: 3 de Octubre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
+def main():
+    num = int(input())
+
+    while num >= 1:
+        num = int(input())
+        num2 = int(num)
+        #num2 = num.append(' ')
+
+        
+        if num == 0:
+            resp = num
+            ans = resp
+
+    if ans == 0:
+        print('Nada')
+    else:
+        print(ans)
+        print(len(num))
+
+
+
+
+
+
+main()
