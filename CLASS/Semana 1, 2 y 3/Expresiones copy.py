2a9810f391f8724d10639fb4c72978d63590c096
Diego Prado Pardo <dpradopardo@gmail.com>
2024-10-09 23:35:36 -0500

    Some adjusts on the files directory

---
+++ CLASS/Semana 1, 2 y 3/Expresiones copy.py
@@ -0,0 +1,15 @@
+edad = int(input('Escribe tu edad: '))
+"""
+a = int(input('Escribe un número para a: '))
+b = int(input('Escribe un número para b: '))
+"""
+segundos = 60
+minutos = segundos * 60
+horas = minutos * 60
+dias = horas * 24
+meses = dias * 30
+año = meses * 12
+añobiciesto = (año%400)
+
+print(añobiciesto)
+
