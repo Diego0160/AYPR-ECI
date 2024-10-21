b24bde04e98d376a8bdcdd558fac51e98602d6cc
Diego0160 <91231627+Diego0160@users.noreply.github.com>
2024-10-21 12:46:57 -0500

    Update mario.py

--- ARENAS/Contest 3/mario.py
+++ ARENAS/Contest 3/mario.py
@@ -8,26 +8,20 @@ Programa: Jumping Mario
 Autor: Diego Prado Pardo
 =========================================
 """
-def calc(arr: list[int]):
-    h = arr[0]
+def calc(arr):
     high, low = 0, 0
-    for height in arr[1:]:
-        if height > h:
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
             high += 1
-        elif height < h:
+        elif arr[i] < arr[i-1]:
             low += 1
-        h = height
-
-    return (high, low)
+    return high, low
 
 def main():
-    t = int(input().strip())
-
-    for i in range(t):
-        n = int(input().strip())
-        nums = [int(x) for x in input().strip().split()]
-
+    for i in range(int(input().strip())):
+        input()  # Ignorar el número de alturas
+        nums = list(map(int, input().strip().split()))
         a, b = calc(nums)
-        print("Case {}: {} {}".format(i+1, a, b))
+        print(f"Case {i+1}: {a} {b}")
 
 main()
