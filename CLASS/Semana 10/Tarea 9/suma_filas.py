11725440ab8ba7d063e11f732b943b8561ee7736
Diego Prado Pardo <dpradopardo@gmail.com>
2024-10-22 17:04:03 -0500

    new changes and files

---
+++ CLASS/Semana 10/Tarea 9/suma_filas.py
@@ -0,0 +1,10 @@
+"""
+=========================================
+AYPR - Algorítmos y programación
+Grupo 61
+Programa: Suma filas
+=========================================
+Fecha: 22 de Octubre de 2024
+Autor: Diego Prado Pardo
+=========================================
+"""
--- CLASS/Semana 10/matrices.py
+++ CLASS/Semana 10/matrices.py
@@ -1,8 +1,25 @@
-# Primer corchete accede a la posicion dentro de la fila
-# Segundo corchete accede a la posicion dentro de la columna
-matriz = [[],[]]
-len(matriz)
+# Definición de una matriz de 2 filas y 3 columnas
+matriz = [
+    [1, 2, 3],  # Fila 1
+    [4, 5, 6]   # Fila 2
+]
+
+# Recorremos la matriz e imprimimos cada elemento
+# Primer for recorre las filas
+for i in range(len(matriz)):
+    # Segundo for recorre las columnas dentro de cada fila
+    for j in range(len(matriz[0])):
+        # Imprimimos el valor en la posición [i][j]
+        print(matriz[i][j], end=" ")  # end=" " para mantener los valores de la misma fila juntos
+    # Cuando termine de recorrer la fila, imprimimos un salto de línea
+    print()
+
+# Ejemplo de cómo añadir más filas o columnas si lo necesitas
+matriz.append([7, 8, 9])  # Añadimos una nueva fila a la matriz
+
+# Imprimimos la matriz nuevamente para ver el cambio
+print("\nMatriz después de añadir una fila:")
 for i in range(len(matriz)):
     for j in range(len(matriz[0])):
-        # Cuando acabe cada fila, hacemos el salto de linea
-        print(matriz[i][j], end = " ")
+        print(matriz[i][j], end=" ")
+    print()
