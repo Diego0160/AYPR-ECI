273c18e4b8e1004d91b64bfe5b2dcf261029b3e5
Diego Prado Pardo <dpradopardo@gmail.com>
2024-10-24 21:44:44 -0500

    some new files

--- CLASS/Semana 10/Tarea 9/Con comentarios/sin_repetidos.py
+++ CLASS/Semana 10/Tarea 9/Con comentarios/sin_repetidos.py
@@ -12,24 +12,37 @@ def sin_repetidos(arr):
     """ Función para eliminar repeticiones en una lista
     (list) -> list"""
 
+    # Verificamos que la lista no esté vacía
     if len(arr) == 0:
         return []
     
+    # Inicializamos la lista de resultado con el primer elemento
     resultado = [arr[0]]
     
+    # Usamos un ciclo for para recorrer la lista
     for i in range(1, len(arr)):
+        # Verificamos si el elemento actual es diferente al anterior
         if arr[i] != arr[i-1]:
             resultado.append(arr[i])
     
     return resultado
 
 def main():
-    entrada = input().split()
-    entrada = [int(x) for x in entrada]
+    # Solicitamos la secuencia al usuario
+    # Split nos ayuda con la separación de los elementos
 
+    entrada = input().split()
+    
+    # Convertimos la entrada a enteros
+    entrada = [int(x) for x in entrada]
+    
+    # Llamamos a la función y obtenemos la lista
     sin_repetidos(entrada)
     salida = sin_repetidos(entrada)
     
+    # Mostramos el resultado como una cadena de números separados por espacio
+    # .join nos ayuda a concatenar los elementos de la lista en una cadena
+    # map nos ayuda a convertir cada elemento a string
     print(" ".join(map(str, salida)))
 
 main()
--- CLASS/Semana 10/Tarea 9/Con comentarios/suma_filas.py
+++ CLASS/Semana 10/Tarea 9/Con comentarios/suma_filas.py
@@ -14,25 +14,37 @@ def suma_filas(matriz):
     Función para sumar los valores de cada fila de una matriz.
     (list de dos dimensiones) -> list de una dimensión
     """
+    # Crea una lista vacía para almacenar las sumas de cada fila
     resultado = []
     
+    # Itera sobre cada fila de la matriz
     for fila in matriz:
+        # Sumar todos los elementos de la fila actual
         suma_fila = sum(fila)
+        # Agrega la suma de la fila a la lista de resultados
         resultado.append(suma_fila)
     
     return resultado
 
 def main():
     m, n = map(int, input().split(','))
+
+    # Inicializa una lista vacía para almacenar la matriz
     matriz = []
-
+    # Repite el siguiente bloque m veces (una vez por cada fila)
     for _ in range(m):
-
+        # Lee una fila, separa los números por comas y los convierte a enteros
         fila = list(map(int, input().split(',')))
+        # Agrega la fila a la matriz
         matriz.append(fila)
-
+    
+    # Llama a la función suma_filas y almacena el resultado
     suma_filas(matriz)
     resultado = suma_filas(matriz)
+    
+    # Convierte cada suma a cadena y las une con comas para imprimir
+    # .join nos ayuda a concatenar los elementos de la lista en una cadena
+    # map nos ayuda a convertir cada elemento a string
     print(",".join(map(str, resultado)))
 
 main()
