41cf7e90efebe3d0fe37675ddc676668d9190365
Diego Prado Pardo <dpradopardo@gmail.com>
2024-10-31 11:27:32 -0500

    new changes

--- CLASS/Semana 11/listas.py
+++ CLASS/Semana 11/listas.py
@@ -8,4 +8,31 @@ Fecha: 31 de Octubre de 2024
 Autor: Diego Prado Pardo
 =========================================
 """
-def 
+def secuencia_valores(v):
+    """ Esta función devuelve una lista con los valores dados por el usuario
+    (list) -> list """
+    lista = print("Punto 2\n", " ".join(v))
+    return lista
+
+def linea(lista):
+    """ 
+     """
+    for v in range(lista):
+        ent = print("Punto 3\n", v)
+    return ent
+
+def cantidad_e(cantidad):
+    """ 
+     """
+    count = print("Punto 4\n",len(cantidad))
+    return count
+
+def main():
+    s_lista = []
+    v = input()
+    s_lista.append(v.split(','))
+    print("Punto 1\n", s_lista)
+    ans2 = secuencia_valores(v)
+    ans3 = linea(v)
+    ans4 = cantidad_e(v)
+main()
