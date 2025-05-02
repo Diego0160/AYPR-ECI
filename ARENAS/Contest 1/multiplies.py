7bff58b780ec6c34306330ac84a8cecf18a5911a
Diego Prado Pardo <dpradopardo@gmail.com>
2025-05-02 11:02:44 -0500

    adjust for branch

--- ARENAS/Contest 1/multiplies.py
+++
@@ -1,7 +0,0 @@
-a = int(input())
-b = int(input())
-
-if a % b == 0 or b % a == 0:
-    print("SI")
-else:
-    print("NO")
--- CLASS/2024-2/Extra/hello-print.py
+++
@@ -1,4 +0,0 @@
-def HelloWorld(str):
-    print(str)
-
-HelloWorld('print')
